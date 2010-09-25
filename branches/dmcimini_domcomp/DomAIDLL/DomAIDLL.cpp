b62b26ecd0081955276beb71b803a65e1bc44bfd
dmcimini <dmcimini@fa5fcfa2-b5ae-668c-4e99-ee49670662f9>
2010-09-25 07:35:16 +0000

    Adding DLL proxy class, IDll, to hide system specific DLL load and function location resolution.

--- branches/dmcimini_domcomp/DomAIDLL/DomAIDLL.cpp
+++ branches/dmcimini_domcomp/DomAIDLL/DomAIDLL.cpp
@@ -1,10 +1,16 @@
 #include "stdafx.h"
 #include "DomAIDLL.h"
 
-DOMAIDLL_API void CreateAI( void )
+DOMAIDLL_API bool CreateAI( Domlib::AI* &pAI )
 {
+    pAI = NULL;
+
+    return true;
 }
 
-DOMAIDLL_API void DestroyAI( void )
+DOMAIDLL_API bool DestroyAI( Domlib::AI* &pAI )
 {
+    pAI = NULL;
+
+    return true;
 }
